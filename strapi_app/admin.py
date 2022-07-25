fb973e3caf63fb4f037a4c8764830fe5f8550ae5
licho08055 <iliyae547@gmail.com>
2022-07-26 00:18:23 +0100

    initial commit

---
+++ strapi_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import People,Planet,Character
+# Register your models here.
+
+admin.site.register(People)
+admin.site.register(Planet)
+admin.site.register(Character)
---
+++ strapi_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StrapiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'strapi_app'
