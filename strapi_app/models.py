8e45965b058ee6a60e8cb36a5de00774f9f7be50
licho08055 <iliyae547@gmail.com>
2022-08-07 06:40:01 +0100

    nin

--- strapi_app/models.py
+++ strapi_app/models.py
@@ -12,7 +12,7 @@ class Planet(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=300)
     code = models.CharField(max_length=20)
-    picture_url = models.CharField(max_length=200)
+    picture_url = models.CharField(max_length=600)
     
     def __str__(self):
         return self.name
@@ -21,7 +21,7 @@ class Planet(models.Model):
 class Character(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=300)
-    picture_url = models.CharField(max_length=200)
+    picture_url = models.CharField(max_length=600)
     planet =  models.ForeignKey(Planet, on_delete=models.CASCADE)
     people = models.ManyToManyField(People)
     
